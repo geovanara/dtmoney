d627356f11f718837fe4d458951e7f5d75d889a1
Geovana Rafa <geovanarafa34@gmail.com>
2022-09-13 16:17:40 -0400

    first commit

---
+++ src/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Server} from 'miragejs'
+import { App } from './App';
+import { createServer } from 'http';
+
+createServer({
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/transactions'), () => {
+      return [
+        {
+          id: 1,
+          title: 'Transaction 1',
+          amount: 'deposit',
+          category: 'Food',
+          createdAt: new Date()
+        }
+      ]
+    }
+  }
+})
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
